09082a27019363811f1fe185956e07e736a8eacd
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-24 17:34:41 -0700

    created a socketio instance by invoking it as a function in server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio();
 
 const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname, "../public");
