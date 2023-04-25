94ab1278cf3ff003f2ba56630a482625830e605e
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-24 20:20:40 -0700

    added a disconnecting event on the server and notified all users about a person leaving the chat

--- src/server.js
+++ src/server.js
@@ -16,11 +16,18 @@ app.use(express.static(publicDirectory));
 io.on("connection", (socket) => {
   // 3. Emit the event
   socket.emit("message", "Welcome!");
+  // Let existing users know that a new person has joined them (the newcomer won't see this message)
+  socket.broadcast.emit("message", "A new user has joined!");
   // 7. Listen to the frontend entered message
   socket.on("sendMessage", (msg) => {
     // 8. Make it visible to all users
     io.emit("message", msg);
   });
+  // 9. Listen for the user disconnection
+  socket.on("disconnect", () => {
+    // 10.Let all remaining users know that the person has left
+    io.emit("message", "A user has left!")
+  })
 });
 
 const start = async () => {
