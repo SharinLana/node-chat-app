f0f42c1af679e21972b55010b7deeb3b86c2c2ad
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-24 21:39:08 -0700

    created a basic setup for sending geolocation to the server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("#form");
 const btn = document.querySelector("#btn");
+const geoBtn = document.querySelector("#geo-btn");
+
 // 2. Establish a connection to the backend
 const socket = io();
 // 4. Receive a first message from the backend
@@ -12,4 +14,15 @@ form.addEventListener("submit", (e) => {
   const inputMessage = e.target.elements.msg.value;
   // 6. Emit the new event, sending the input value to the backend
   socket.emit("sendMessage", inputMessage);
+});
+
+// Sharing the user's location
+geoBtn.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Your browser does not support geolocation");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+  });
 });
