c7f225e2eef30ac14ec237df02636f46310efab4
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-26 15:29:44 -0700

    exported the util functions in the users.js

--- src/utils/users.js
+++ src/utils/users.js
@@ -32,15 +32,6 @@ const removeUser = (id) => {
 
 const getUser = (id) => users.find((user) => user.id === id);
 
-const getUsersInRoom = (room) => users.filter((user) => user.room === room)
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-// addUser({ id: 1, username: "Michael", room: "chat" });
-// addUser({ id: 2, username: "Sophia", room: "chat" });
-// console.log(users);
-
-// console.log(removeUser(1));
-// console.log(users);
-addUser({ id: 1, username: "Michael", room: "chat" });
-addUser({ id: 2, username: "Sophia", room: "chat" });
-console.log(getUser(1));
-console.log(getUsersInRoom("chat"));
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
